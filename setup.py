a1a43155b4dde7ec92ef5bb3fdc2de4cfc3e7d09
Philip Bauer <bauer@starzel.de>
2015-11-30 14:29:54 +0100

    Preparing release 1.0b1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (
 
 setup(
     name='collective.behavior.banner',
-    version='1.0a1',
+    version='1.0b1',
     description="A behavior to create sliders with banners",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers

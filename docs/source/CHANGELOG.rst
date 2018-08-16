4469d673210acafff4c6ccea708b68d5f142eade
Philip Bauer <bauer@starzel.de>
2018-08-16 08:54:20 +0200

    Back to development: 1.1

--- docs/source/CHANGELOG.rst
+++ docs/source/CHANGELOG.rst
@@ -3,6 +3,12 @@
 Changelog
 =========
 
+1.1 (unreleased)
+----------------
+
+- Nothing changed yet.
+
+
 1.0 (2018-08-16)
 ----------------
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.behavior.banner',
-    version='1.0',
+    version='1.1.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers

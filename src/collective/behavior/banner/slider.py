36bdf3f02a1f33dafaaba2581a56ed2a8733d6d1
Philip Bauer <bauer@starzel.de>
2021-01-27 21:07:50 +0100

    fix use with multilingual setup

--- src/collective/behavior/banner/slider.py
+++ src/collective/behavior/banner/slider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from collective.behavior.banner import _
+from plone.app.layout.navigation.root import getNavigationRoot
 from plone.app.vocabularies.catalog import CatalogSource
 from plone.app.z3cform.widget import RelatedItemsFieldWidget
 from plone.autoform import directives
@@ -9,10 +10,11 @@ from plone.supermodel import model
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
 from zope.component import adapter
-from zope.interface import alsoProvides
+from zope.interface import provider
 from zope.interface import implementer
 
 
+@provider(IFormFieldProvider)
 class ISlider(model.Schema):
 
     model.fieldset(
@@ -28,7 +30,6 @@ class ISlider(model.Schema):
         description=_(u'These banners will be used in the slider'),
         default=[],
         value_type=RelationChoice(
-            title=_(u'Target'),
             source=CatalogSource(banner_has_image=True)
         ),
         required=False,
@@ -37,11 +38,10 @@ class ISlider(model.Schema):
         'slider_relation',
         RelatedItemsFieldWidget,
         pattern_options={
+            'basePath': getNavigationRoot,
             'mode': 'search',
             }
         )
-
-alsoProvides(ISlider, IFormFieldProvider)
 
 
 @implementer(ISlider)

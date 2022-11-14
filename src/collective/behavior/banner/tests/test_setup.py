9d8a14740ee4c924c5e5ed3fa6c7290e66dd1136
Peter Mathis <peter.mathis@kombinat.at>
2022-11-14 09:32:01 +0100

    Plone 6 compatible test setup

--- src/collective/behavior/banner/tests/test_setup.py
+++ src/collective/behavior/banner/tests/test_setup.py
@@ -3,18 +3,12 @@ from collective.behavior.banner.testing import COLLECTIVE_BEHAVIOR_BANNER_INTEGR
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
+from plone.base.utils import get_installer
 from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.interfaces import IResourceRegistry
 from zope.component import getUtility
 
 import unittest
-
-
-try:
-    from Products.CMFPlone.utils import get_installer
-    has_get_installer = True
-except ImportError:
-    has_get_installer = False
 
 
 CSS = (
@@ -31,14 +25,11 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        if has_get_installer:
-            self.installer = get_installer(self.portal, self.layer['request'])
-        else:
-            self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.installer = get_installer(self.portal, self.layer['request'])
 
     def test_product_installed(self):
         """Test if collective.behavior.banner is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
+        self.assertTrue(self.installer.is_product_installed(
             'collective.behavior.banner'))
 
     def test_browserlayer(self):
@@ -69,18 +60,15 @@ class TestUninstall(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
-        if has_get_installer:
-            self.installer = get_installer(self.portal, self.layer['request'])
-        else:
-            self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.installer = get_installer(self.portal, self.layer['request'])
         roles_before = api.user.get_roles(TEST_USER_ID)
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.installer.uninstallProducts(['collective.behavior.banner'])
+        self.installer.uninstall_product('collective.behavior.banner')
         setRoles(self.portal, TEST_USER_ID, roles_before)
 
     def test_product_uninstalled(self):
         """Test if collective.behavior.banner is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled(
+        self.assertFalse(self.installer.is_product_installed(
             'collective.behavior.banner'))
 
     def test_browserlayer_removed(self):
